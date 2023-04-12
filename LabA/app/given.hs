2f14b755739e29aed0a4abb07411ce4489c6ee69
ale99WGiais <alessiocorrado99@gmail.com>
2023-04-12 21:45:43 +0200

    parte 3 finita

--- LabA/app/given.hs
+++ LabA/app/given.hs
@@ -11,8 +11,9 @@ main = do
   file <- readFile f
 
   let puzzles   = lines file
+      solutions = map solve puzzles
       --solutions = runEval (parMap solve puzzles)
-      solutions = parMapBuffer 10 solve puzzles
+      --solutions = parMapBuffer 100 solve puzzles
 
   evaluate (length puzzles)
   print (length (filter isJust solutions))
@@ -29,4 +30,5 @@ parMapBuffer :: NFData b => Int -> (a -> b) -> [a] -> [b]
 parMapBuffer _ _ [] = []
 --parMapBuffer n f xs = withStrategy (parBuffer n rseq) $ map f xs
 --parMapBuffer n f xs = withStrategy (parBuffer n rseq) $ (map f xs `using` parListChunk 10 rdeepseq)
-parMapBuffer n f xs = map f xs `using` parListChunk n rdeepseq
+--parMapBuffer n f xs = map f xs `using` parListChunk n rdeepseq
+parMapBuffer n f xs = map f xs `using` parList rdeepseq
