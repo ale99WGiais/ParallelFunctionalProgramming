52c1a801852e409cca18cb54a4a9065718f52709
ale99WGiais <alessiocorrado99@gmail.com>
2023-05-17 22:19:46 +0200

    ok

--- LabD/page_rank.erl
+++ LabD/page_rank.erl
@@ -8,8 +8,11 @@
 %% Use map_reduce to count word occurrences
 
 map(Url,ok) ->
-    [{Url,Body}] = dets:lookup(web,Url),
+    %%io:format("mapped " ++ Url ++ "\n"),
+    dets:open_file(web2,[{file,"web.dat"}]),
+    [{Url,Body}] = dets:lookup(web2,Url),
     Urls = crawl:find_urls(Url,Body),
+    dets:close(web2),
     [{U,1} || U <- Urls].
 
 reduce(Url,Ns) ->
@@ -21,9 +24,19 @@ page_rank() ->
     map_reduce:map_reduce_seq(fun map/2, fun reduce/2, 
 			      [{Url,ok} || Url <- Urls]).
 
+
 page_rank_par() ->
     dets:open_file(web,[{file,"web.dat"}]),
     Urls = dets:foldl(fun({K,_},Keys)->[K|Keys] end,[],web),
     map_reduce:map_reduce_par(fun map/2, 32, fun reduce/2, 32, 
 			      [{Url,ok} || Url <- Urls]).
 
+
+-define(EXECUTIONS,5).
+
+bm(F) ->
+    {T,_} = timer:tc(?MODULE,repeat,[F]),
+    T/?EXECUTIONS/1000.
+
+repeat(F) ->
+    [F() || _ <- lists:seq(1,?EXECUTIONS)].
